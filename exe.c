2e4b9c07f0264a38024da71f8bf1893705e4c959
WairegiWhitney <wairegiwhitney11@gmail.com>
2024-02-24 08:43:24 +0300

    tried to tokenize the arguments

--- exe.c
+++ exe.c
@@ -10,9 +10,26 @@ void exe(char *input) {
         perror("fork");
         exit(EXIT_FAILURE);
     } else if (child_pid == 0) {
-        printf("Child process ID: %d\n", getpid());
+       // printf("Child process ID: %d\n", getpid());
+       char *my_args[120];
+       int count=0;
+
+       char *token = strtok((char*)input," ");
+       while (token != NULL)
+       {
+        my_args[count++]=token;
+        token = strtok(NULL, " ");
+       }
+       my_args[count]=NULL;
+        //execute command
+       execvp(my_args[0],my_args);
+
+       cout("error executing command. \n");
+       exit(EXIT_FAILURE);
+       
     } else {
-		printf("Parent process ID: %d\n", getppid());
+        //parent waits until the child is done executing
+		wait(NULL);
         
     }
 	free(input);
