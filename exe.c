7a1c73a9fe84b545f634a05cfe280bcf3f32cf2e
Calvince Ndemo <calyndemo16@gmail.com>
2024-02-26 13:09:11 +0300

    made the first working terminal by completing the .exe

--- exe.c
+++ exe.c
@@ -2,78 +2,46 @@
 
 void exe(char *input) {
 
-	char ** argv = {NULL};
-	
-	/* char *cmd; */
-    pid_t nxt_pid = fork();
-	/* print input using printf */
-	printf("input: %s\n", input);
-	/* print nxt_pid using printf */
-	printf("nxt_pid: %d\n", nxt_pid);
 
 
-    if (nxt_pid == -1) {
+    pid_t child_pid = fork(); 
+
+    if (child_pid == -1) {
         cout("Error forking process.\n");
         exit(0);
-    } else if (nxt_pid == 0) {
-		/*child process*/
-		int i = 0;
-		int j = 0;
-        char *token;
-		char input2[1080];
-		replace(input, input2, "");
-		token = strtok((char *)input2, " ");
+    } else if (child_pid == 0) {
+       /*child process*/
+       char *argvec[120];
+       int count=0;
 
-		replace(input, input2, token);
-		printf("token: %s\n", token);
-
-		/* cmd = token; */
-
-
-		/* print input using printf */
-		cout("input: ");
-		cout(input);
-		cout("\n");
-
-		cout("input2: ");
-		cout(input2);
-		cout("\n");
+       char *token = strtok((char*)input," ");
+       while (token != NULL)
+       {
+        argvec[count++]=token;
+        token = strtok(NULL, " ");
+       }
+       argvec[count]=NULL;
+	   /*print my_args*//*
+	   cout("argvec: ");
+	   for(; i<count; i++){
+		   cout(argvec[i]);
+		   cout("\n");
+	   }
+	   */
+	   
+        /* execute command */
+		if (argvec[0] == NULL)
+			exit(0);
 		
-		cout("token: ");
-		cout(token);
-		cout("\n");
+       execve(argvec[0], argvec, NULL);
 
-
-
-        while (token != NULL) {
-			cout("token: ");
-			cout(token);
-			cout("\n");
-			argv[i++] = token;
-            token = strtok(NULL, " ");
-        }
-
-		argv[i] = NULL;
-		
-		for (; j < i; j++)
-		{
-			cout(argv[j]);
-		}
-		execve(argv[0], argv, NULL);
-		cout("Error executing command.\n");
-		exit(0);
-		
+       cout("error executing command. \n");
+       exit(0);
+       
     } else {
-		/*parent process*/
-		wait(NULL);        
+        /* parent process */
+		wait(NULL);
+        
     }
-	
 
 }
-
-
-
-/**
- * SUGGESTED IMPROVEMENTS/ POSSIBLE BUGS
- * replace strdup with custom function
-*/
