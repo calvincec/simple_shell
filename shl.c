b5b4d3454b489a00ffa1dc0daba5c23114362532
Calvince Ndemo <calyndemo16@gmail.com>
2024-03-07 15:48:44 +0300

    3/7/2024 betties corrected

--- shl.c
+++ shl.c
@@ -1,7 +1,7 @@
 #include "shl.h"
 
 /**
- * main - entry point
+ * mainy - entry point
  * Return: 0 on success
  */
 int mainy(void)
@@ -28,11 +28,15 @@ int mainy(void)
 	return (ret);
 }
 
+/**
+ * mainyo - entry point
+ * Return: 0 on success
+ */
 int mainyo(void)
 {
 	char input[1024];
 
-	while(1)
+	while (1)
 	{
 		printf("$ ");
 		fgets(input, 1024, stdin);
@@ -40,24 +44,36 @@ int mainyo(void)
 	return (0);
 }
 
-int mainyoo() {
-    int is_piped = !isatty(fileno(stdin));
+/**
+ * mainyoo - entry point
+ * Return: 0 on success
+ */
+int mainyoo(void)
+{
+	int is_piped = !isatty(fileno(stdin));
 
-    if (is_piped) {
-        char line[1024];
+	if (is_piped)
+	{
+		char line[1024];
 
-        printf("Input is coming from a pipe.\n");
+		printf("Input is coming from a pipe.\n");
 
-        while (fgets(line, 1024, stdin) != NULL) {
-            printf("Line: %s", line);
-        }
-    } else {
-        printf("Input is coming from keyboard.\n");
-    }
+		while (fgets(line, 1024, stdin) != NULL)
+		{
+			printf("Line: %s", line);
+		}
+	} else
+	{
+		printf("Input is coming from keyboard.\n");
+	}
 
-    return 0;
+	return (0);
 }
 
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int ret;
@@ -68,25 +84,22 @@ int main(void)
 	ret = 0;
 	input = (char *) malloc(size + 1);
 
-	if(is_piped)
+	if (is_piped)
 	{
 		char *commands[1024];
 		int i = 0;
 		int p;
-		
+
 		while (fgets(input, 1024, stdin) != NULL)
 		{
 			commands[i] = input;
 			i++;
 		}
-		
-
 		commands[i] = NULL;
 		p = 0;
-		for(;p<i;p++)
+		for ( ; p < i ; p++)
 		{
 			removenewln(commands[p]);
-
 			ret = exe(commands[p]);
 		}
 	}
@@ -100,9 +113,7 @@ int main(void)
 			ret = exe(input);
 		} while (1);
 	}
-
 	free(input);
-
 	return (ret);
 }
 
